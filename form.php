cc11698607440e80cec9817bb173b486ae1ff298
BlueMatrixx <91186167+BlueMatrixx@users.noreply.github.com>
2022-08-06 18:44:10 +0530

    Update form.php

--- form.php
+++ form.php
@@ -13,7 +13,7 @@ $email_body="User Name: $name.\n".
 "Subject: $subject.\n".
 "User Message: $message.\n";
 
-$to = 'aniketpatel6264@gmail.com';
+$to = 'udittatisar@gmail.com';
 
 $headers = "From: $email_from  \r\n";
 
